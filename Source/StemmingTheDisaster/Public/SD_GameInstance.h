ef72c19c4a953d5f6e5a543ee3e7c58e7d392ada
Andrew Ragland <agragland00@gmail.com>
2022-03-07 17:37:51 -0600

    Added SD_GameInstance
    Added SD_GameInstance class and added it to the GameInstance within project settings.

---
+++ Source/StemmingTheDisaster/Public/SD_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SD_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEMMINGTHEDISASTER_API USD_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
