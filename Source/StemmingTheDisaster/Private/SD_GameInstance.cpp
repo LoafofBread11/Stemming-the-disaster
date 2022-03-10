18a181b5416cace87c79ff82a171aaa79d3e9575
Andrew Ragland <agragland00@gmail.com>
2022-03-10 14:10:35 -0600

    Declared all gameInstance methods

--- Source/StemmingTheDisaster/Private/SD_GameInstance.cpp
+++ Source/StemmingTheDisaster/Private/SD_GameInstance.cpp
@@ -3,3 +3,73 @@
 
 #include "SD_GameInstance.h"
 
+USD_GameInstance::USD_GameInstance()
+{
+
+}
+
+void USD_GameInstance::StartSimulator()
+{
+	return;
+}
+
+void USD_GameInstance::EndSimulator()
+{
+	return;
+}
+
+void USD_GameInstance::SetupMap()
+{
+	return;
+}
+
+int USD_GameInstance::GetInvestment(FString item)
+{
+	return 0;
+}
+
+void USD_GameInstance::SetInvestment(FString item, int money)
+{
+	return;
+}
+
+bool USD_GameInstance::UpdateInvestment(FString item, int money)
+{
+	return true;
+}
+
+TArray<FString> USD_GameInstance::GetDialouge()
+{
+	return dialougeOptions;
+}
+
+void USD_GameInstance::GenerateDialouge(FString dialogue)
+{
+	return;
+}
+
+TArray<FString> USD_GameInstance::GetMaps()
+{
+	return travelableMaps;
+}
+
+void USD_GameInstance::ChangeMap(int selection)
+{
+	return;
+}
+
+FString USD_GameInstance::GetCurrentAction()
+{
+	return currentAction;
+}
+
+void USD_GameInstance::SetCurrentAction(FString action)
+{
+	currentAction = action;
+}
+
+void USD_GameInstance::SetInteractableData(FText name, FText desc)
+{
+	return;
+}
+
--- Source/StemmingTheDisaster/Public/SD_GameInstance.h
+++ Source/StemmingTheDisaster/Public/SD_GameInstance.h
@@ -6,6 +6,8 @@
 #include "Engine/GameInstance.h"
 #include "SD_GameInstance.generated.h"
 
+
+
 /**
  * 
  */
@@ -13,5 +15,84 @@ UCLASS()
 class STEMMINGTHEDISASTER_API USD_GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+	public:
+
+		USD_GameInstance();
+
+		UPROPERTY(VisibleAnywhere)
+		FString currentAction; 
+
+		UPROPERTY(VisibleAnywhere)
+		FText interactableText;
+
+		UPROPERTY(VisibleAnywhere)
+		FText interactableName;
+
+		UPROPERTY(VisibleAnywhere)
+		TMap <FString, int> careerPathScores; //scoreList in LucidChart
+
+		UPROPERTY(VisibleAnywhere)
+		TMap <FString, int> investmentOptions;
+
+		UPROPERTY(VisibleAnywhere)
+		TArray <FString> dialougeOptions;
+
+		UPROPERTY(VisibleAnywhere)
+		TArray <FString> results;
+
+		UPROPERTY(VisibleAnywhere)
+		TArray <FString> travelableMaps; //array of all maps that can be traveled to
+
+		UPROPERTY(VisibleAnywhere)
+		float remainingTime; //amount of time remaining
+
+		UPROPERTY(VisibleAnywhere)
+		int remainingCurrency; //amount of currency remaining 
+
+		UFUNCTION(BlueprintCallable)
+		void setRemainingTime(float time) { remainingTime = time; }
+
+		UFUNCTION(BlueprintCallable)
+		float getRemainingTime() { return remainingTime; }
+
+		UFUNCTION()
+		void StartSimulator();
+
+		UFUNCTION()
+		void EndSimulator();
+
+		UFUNCTION()
+		void SetupMap();
+
+		UFUNCTION()
+		int GetInvestment(FString item);
+
+		UFUNCTION()
+		void SetInvestment(FString item, int money);
+
+		UFUNCTION()
+		bool UpdateInvestment(FString item, int money);
+
+		UFUNCTION()
+		TArray<FString> GetDialouge();
+
+		UFUNCTION()
+		void GenerateDialouge(FString dialouge);
+
+		UFUNCTION()
+		TArray<FString> GetMaps();
+
+		UFUNCTION()
+		void ChangeMap(int selection);
+
+		UFUNCTION()
+		FString GetCurrentAction();
+
+		UFUNCTION()
+		void SetCurrentAction(FString action);
+
+		UFUNCTION()
+		void SetInteractableData(FText name, FText desc);
+
 };
