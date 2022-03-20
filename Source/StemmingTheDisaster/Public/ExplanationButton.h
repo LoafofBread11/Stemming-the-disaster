514f47bd546edc404b41970115f25ae79dc35aa1
zodiak217 <jaschultz40@gmail.com>
2022-03-20 11:09:11 -0500

    Created empty button subclasses.

---
+++ Source/StemmingTheDisaster/Public/ExplanationButton.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ButtonMain.h"
+
+class STEMMINGTHEDISASTER_API AExplanationButton : public AButtonMain
+{
+
+};
