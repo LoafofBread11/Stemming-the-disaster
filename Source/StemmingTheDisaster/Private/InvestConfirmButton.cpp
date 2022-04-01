d2de85f553b168325afc3347da3b9b1c811c19ee
zodiak217 <jaschultz40@gmail.com>
2022-04-01 10:23:27 -0500

    Pushed Invest confirm button.

--- Source/StemmingTheDisaster/Private/InvestConfirmButton.cpp
+++ Source/StemmingTheDisaster/Private/InvestConfirmButton.cpp
@@ -3,3 +3,35 @@
 
 #include "InvestConfirmButton.h"
 
+AInvestConfirmButton::AInvestConfirmButton()
+{
+	//constructor
+}
+
+void AInvestConfirmButton::BeginPlay()
+{
+	Super::BeginPlay();
+	GI = Cast<USD_GameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+}
+
+void AInvestConfirmButton::raiseFlag()
+{
+	flag = 1;
+}
+
+void AInvestConfirmButton::click()
+{
+	Super::click();
+	GI->SetCurrentAction("INVEST");
+	raiseFlag();
+}
+
+void AInvestConfirmButton::setItem(FString itemName)
+{
+	item = itemName;
+}
+
+void AInvestConfirmButton::setItemDesc(TextComponent newItemDesc)
+{
+	itemDesc = newItemDesc;
+}
--- Source/StemmingTheDisaster/Public/InvestConfirmButton.h
+++ Source/StemmingTheDisaster/Public/InvestConfirmButton.h
@@ -4,8 +4,46 @@
 
 #include "CoreMinimal.h"
 #include "ButtonMain.h"
+#include "InvestConfirmButton.generated.h"
 
 class STEMMINGTHEDISASTER_API AInvestConfirmButton : public AButtonMain
 {
+	GENERATED_BODY()
 
+	public:
+		//Sets default values.
+		AInvestConfirmButton();
+
+	protected:
+		// Called when the game starts or when spawned
+		virtual void BeginPlay() override;
+
+	public:
+		//Variable to hold the name of the item.
+		UPROPERTY(VisibleAnywhere)
+		FString item;
+
+		//Variable to hold the description of the item.
+		UPROPERTY(VisibleAnywhere)
+		TextComponent itemDesc;
+
+		//Flag for the button
+		UPROPERTY(VisibleAnywhere)
+		int flag = -1;
+
+		//Copy of the game instance.
+		UPROPERTY(VisibleAnywhere)
+		USD_GameInstance* GI;
+
+		//Function to raise the flag once the button is pressed.
+		virtual void raiseFlag() override;
+
+		//Function for when the button is clicked.
+		void click() override;
+
+		//Function to set the item's name.
+		void setItem(FString itemName);
+
+		//Funtion to set the item's description.
+		void setItemDesc(TextComponent newItemDesc);
 };
